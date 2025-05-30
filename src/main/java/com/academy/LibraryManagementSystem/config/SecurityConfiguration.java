0a7ee5dccc9059cfd0d5990c8664fd93684078f3
Ivanalem <145138223+Ivanalem@users.noreply.github.com>
2025-05-31 02:03:22 +0300

    n

--- src/main/java/com/academy/LibraryManagementSystem/config/SecurityConfiguration.java
+++ src/main/java/com/academy/LibraryManagementSystem/config/SecurityConfiguration.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration implements SecurityContext {
+public class SecurityConfiguration {
 
 
 
